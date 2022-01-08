3015cdc1ccd61417c383cf062a51e9a102954ee1
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2022-01-08 20:27:16 +0300

    Initial Commit

---
+++ Eng2GO_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eng2GO_app
+//
+//  Created by Константин Кириллов on 08.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eng2GO_app/Eng2GO_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eng2GO_appApp.swift
+//  Eng2GO_app
+//
+//  Created by Константин Кириллов on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Eng2GO_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
